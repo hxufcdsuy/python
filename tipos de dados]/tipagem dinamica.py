1efc914fce2bf235a58a57d393c34cb86a5f23aa
hxufcdsuy <161042520+hxufcdsuy@users.noreply.github.com>
2024-05-03 12:24:15 +0000

    primeira parte

---
+++ tipos de dados]/tipagem dinamica.py
@@ -0,0 +1,14 @@
+#tipagem dinamica
+#atribuindo valores
+nome = "Marta"
+idade = 20
+peso = 62.500
+#exibindo dados
+print(nome)
+print(idade)
+print(peso)
+
+#formatado
+print(f"nome : {nome}")
+print(f"idade: {idade}")
+print(f"peso : {peso}")
---
+++ tipos de dados]/tipagem estática.py
@@ -0,0 +1,17 @@
+
+#tipagem estática
+
+
+
+nome: str = "Marta"
+idade: int = 20
+peso: float = 62.500
+#exibindo dados
+print(nome);
+print(idade);
+print(peso);
+
+#formatado
+print(f"nome : {nome}");
+print(f"idade: {idade}");
+print(f"peso : {peso}");
